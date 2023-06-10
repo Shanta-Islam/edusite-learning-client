05fc5dcda9fea5d013c29603b8e104989317033a
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-06-10 19:09:36 +0600

    course details page create and show data

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
+import Courses from "../Pages/Shared/Courses/Courses";
+import CourseDetails from "../Pages/CourseDetails/CourseDetails";
 
 export const routes = createBrowserRouter([
     {
@@ -16,6 +18,16 @@ export const routes = createBrowserRouter([
                 path: "/home",
                 loader: ()=> fetch("https://edusite-learning-server.vercel.app/courses"),
                 element: <Home></Home>
+              },
+              {
+                path: "/courses",
+                loader: ()=> fetch("https://edusite-learning-server.vercel.app/courses"),
+                element: <Courses></Courses>
+              },
+              {
+                path: "/course/:id",
+                loader: ({ params }) => fetch(`https://edusite-learning-server.vercel.app/course/${params.id}`),
+                element: <CourseDetails></CourseDetails>
               },
         ]
       }
